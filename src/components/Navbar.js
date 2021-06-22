6cc79bd37342275d587989134f558b1b19fda54e
daphnemagsby <daphnemagsby@gmail.com>
2021-06-21 21:13:14 -0500

    Add styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {MenuItems} from './MenuItems';
 
+
 const Navbar = () => {
 	return(
 		<nav className='NavbarItems'>
-			<h1 className='navbar-logo'>React</h1>
+			<h1 className='navbar-logo'>React <i className="fab fa-react"></i></h1>
 			<div className='menu-icon'>
 			
 			</div>
