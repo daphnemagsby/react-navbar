01b00d7d87a0059e43c26bbbad0d0d2064e45e5e
daphnemagsby <daphnemagsby@gmail.com>
2021-06-22 22:08:35 -0500

    Remove Button.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {MenuItems} from './MenuItems';
-
+import '../styles/Navbar.css';
 
 const Navbar = () => {
 	return(
-		<nav className='NavbarItems'>
+		<nav className='navbar-items'>
 			<h1 className='navbar-logo'>React <i className="fab fa-react"></i></h1>
 			<div className='menu-icon'>
 			
