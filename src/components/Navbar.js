1c792a95dd064300a2a85af97916be4f72c92a98
daphnemagsby <daphnemagsby@gmail.com>
2021-06-22 23:13:50 -0500

    Add state for hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {MenuItems} from './MenuItems';
 import '../styles/Navbar.css';
 
+
 const Navbar = () => {
+	const [click, setClick] = useState(false);
+
 	return(
 		<nav className='navbar-items'>
 			<h1 className='navbar-logo'>React <i className="fab fa-react"></i></h1>
-			<div className='menu-icon'>
-			
+			<div className='menu-icon' onClick={()=>{setClick(!click)}}>
+				<i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
 			</div>
-			<ul>
+			<ul className={click ? 'nav-menu active' : 'nav-menu'}>
 				{MenuItems.map((item, index)=>(
 					<li key={index}>
 						<a className={item.className} href={item.href}> {item.title}</a>
